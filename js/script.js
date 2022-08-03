6fadd2fd23542e68d3ba6b457487d70d4a1b5c45
biszaal <biszaal@yahoo.com>
2022-08-03 15:11:53 +0545

    added js click button

--- js/script.js
+++ js/script.js
@@ -25,4 +25,37 @@ function fadeOut() {
   setInterval(loader, 3000);
 }
 
+function lunchMenu() {
+  var dinner_menu = document.getElementById("dinner-menu");
+  var drink_menu = document.getElementById("drink-menu");
+  dinner_menu.style.display = "none";
+  drink_menu.style.display = "none";
+
+  var lunch_menu = document.getElementById("lunch-menu");
+  lunch_menu.style.display = "block";
+  lunch_menu.scrollIntoView({ behavior: "smooth" }, true);
+}
+
+function dinnerMenu() {
+  var lunch_menu = document.getElementById("lunch-menu");
+  var drink_menu = document.getElementById("drink-menu");
+  lunch_menu.style.display = "none";
+  drink_menu.style.display = "none";
+
+  var dinner_menu = document.getElementById("dinner-menu");
+  dinner_menu.style.display = "block";
+  dinner_menu.scrollIntoView({ behavior: "smooth" }, true);
+}
+
+function drinkMenu() {
+  var lunch_menu = document.getElementById("lunch-menu");
+  var dinner_menu = document.getElementById("dinner-menu");
+  lunch_menu.style.display = "none";
+  dinner_menu.style.display = "none";
+
+  var drink_menu = document.getElementById("drink-menu");
+  drink_menu.style.display = "block";
+  drink_menu.scrollIntoView({ behavior: "smooth" }, true);
+}
+
 window.onload = fadeOut();
